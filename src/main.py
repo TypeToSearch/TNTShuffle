9e3b556968075aca036c4c58293e23fd5c2e4fab
Ben <thomben4@isu.edu>
2025-10-15 18:48:13 -0600

    Basic OAuth working

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
 import random
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import dotenv_values
 
-# Replace these with your app credentials
+CONFIG = dotenv_values(".env")
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id='YOUR_CLIENT_ID',
-    client_secret='YOUR_CLIENT_SECRET',
-    redirect_uri='http://localhost:8888/callback',
+    client_id=CONFIG["CLIENT_ID"],
+    client_secret=CONFIG["CLIENT_SECRET"],
+    redirect_uri='http://127.0.0.1:8888/callback',
     scope='user-read-playback-state user-modify-playback-state playlist-read-private'
 ))
+
+current = sp.current_playback()
+print(current)
+
